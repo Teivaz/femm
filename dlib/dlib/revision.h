4fc4bdfc60369afb838f998ede50ab8af8f2acfa
teivaz <github@teivaz.com>
2018-06-18 19:44:42 +0200

    Added dlib

---
+++ dlib/dlib/revision.h
@@ -0,0 +1,8 @@
+#ifndef DLIB_REVISION_H
+// Version:  19.13
+// Date:     Sat May 26 07:56:42 EDT 2018
+// Mercurial Revision ID:  f94b92a017bf
+#define DLIB_MAJOR_VERSION  19
+#define DLIB_MINOR_VERSION  13
+#define DLIB_PATCH_VERSION  0
+#endif
